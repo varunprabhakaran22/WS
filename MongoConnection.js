0e3874d6bc3402007a1c5d5b967ad5ab187b3360
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-10-03 16:07:46 +0530

    connecting with mongo atlas

--- MongoConnection.js
+++ MongoConnection.js
@@ -1,2 +1,5 @@
 const express =require('express');
 const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://Satyajit:1234@cluster0-xulor.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri,{ useUnifiedTopology: true }, { useNewUrlParser: true });
+const app = express();
