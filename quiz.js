740ce6eee61b177791e756220e44162fddc2f1c0
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-10-03 21:23:16 +0530

    ajax con

--- quiz.js
+++ quiz.js
@@ -4,3 +4,13 @@ let answer = '';
 let score = 0;
 let arr=[];
 var testdata;
+$(document).ready(function(){
+    $.ajax({
+        url:'http://localhost:8100/api',
+        
+    }).done(function(data) {
+        //this.data = dataNew;
+        getNextQues(data);
+        $('#next').click(function(){
+            getNextQues(data);
+        });
