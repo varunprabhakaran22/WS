8b0b36f3264e6e10cb3401036c742274ca69795f
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-10-03 16:08:28 +0530

    creating the ports

--- MongoConnection.js
+++ MongoConnection.js
@@ -3,3 +3,5 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://Satyajit:1234@cluster0-xulor.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri,{ useUnifiedTopology: true }, { useNewUrlParser: true });
 const app = express();
+const port = 8100;
+const cors=require('cors');
